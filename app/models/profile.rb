a4d6d8c76f1fdaeff0e7ea94ed16516120952d4a
大胡晴彦 <hd17tennis@icloud.com>
2018-03-11 21:13:28 +0900

    formの修正とデフォルトで入力例が出るようにしました。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,10 @@
 class Profile < ApplicationRecord
   validates :company_id, presence: true
   validates :salary, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
   validates :description, presence: true
   validates :image, presence: true
-  validates :address, presence: true 
+  validates :address, presence: true
   mount_uploader :image, AvatarUploader
 
   belongs_to :company
